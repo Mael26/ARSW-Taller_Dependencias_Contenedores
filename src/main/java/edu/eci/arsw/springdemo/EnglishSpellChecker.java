557e0aee7a3fc9eaf437e8346116b8f87dd47aa5
Mael26 <miguelmonroy@live.com>
2025-09-05 22:28:02 -0500

    Cambios para el uso de EnglishSpellChecker

---
+++ src/main/java/edu/eci/arsw/springdemo/EnglishSpellChecker.java
@@ -0,0 +1,17 @@
+package edu.eci.arsw.springdemo;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@Primary
+public class EnglishSpellChecker implements SpellChecker {
+
+	@Override
+	public String checkSpell(String text) {		
+		return "Checked with english checker:"+text;
+	}
+
+        
+}
